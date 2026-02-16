21f6d7eb31521eb7d086a734dd31c31bc62db0f6
Adi <adivania@yahoo.com.br>
2026-02-16 22:31:27 +0000

    ex 4

---
+++ ex.4.py
@@ -0,0 +1,32 @@
+frase = "Ela estuda bastante"
+print(frase)
+
+nome = "Adivania Pinheiro"
+print(len(nome))
+
+frase_1 = "Vou ser engenheira de IA" 
+frase_2 = " em 2026."
+print(frase_1 + frase_2)
+
+contador = frase_1.count("Vou")
+print(contador)
+
+frase_maiuscula = frase_1.upper()
+print(frase_maiuscula)
+
+nome_invertido = nome[::-1]
+print(nome_invertido)
+
+frase_separada = frase.split()
+print(frase_separada)
+
+frase_a = frase.count("a")
+print(frase_a)
+
+
+frase_11 = frase. split()[1]
+print(frase_11)
+frase_12 = frase.split()[2] 
+print(frase_12)
+frase_inv = frase_1[::-1]
+print(frase_inv)
