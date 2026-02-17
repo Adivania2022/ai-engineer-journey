dc5c0dcb744eda0a332f490f10ed790e3231174d
Adi <adivania@yahoo.com.br>
2026-02-17 12:02:27 +0000

    ex.5.py

---
+++ ex.5.py
@@ -0,0 +1,27 @@
+numero = 10
+numero_decimal = 3.14
+soma = numero + numero_decimal
+texto = "5"
+numero_convertido = int(texto)
+print(numero_convertido)
+texto_decimal = "2.6"
+decimal_convertido = float(texto_decimal)
+print(decimal_convertido)
+numero_arredondado = round(numero_decimal)
+print(numero_arredondado)
+valor_absoluto = abs(numero_decimal)
+print(valor_absoluto)
+import math
+raiz_quadrada = math.sqrt(16)
+print(raiz_quadrada)
+pot_2 = math.pow(2,3)
+print(pot_2)
+print(math.pow(2,3))
+print(math.floor(3.7))
+print(math.ceil(3.2))
+pi= math.pi
+print(pi)
+numeros =[1,2,3,4,5]
+print(sum(numeros))
+print(max(numeros))
+print(min(numeros))
