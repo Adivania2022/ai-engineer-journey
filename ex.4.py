002fe94dae81e74c2e3f9f6b17ccbeaaec878abd
Adi <adivania@yahoo.com.br>
2026-02-17 09:23:02 +0000

    ex.4

--- ex.4.py
+++ ex.4.py
@@ -1,32 +1,30 @@
-frase = "Ela estuda bastante"
+frase = "Hoje é feriado"
 print(frase)
-
-nome = "Adivania Pinheiro"
+nome = "Adivania de Souza Pinheiro"
 print(len(nome))
-
-frase_1 = "Vou ser engenheira de IA" 
-frase_2 = " em 2026."
+email = "adivania@yahoo.com.br"
+print("a" in email)
+frase_1 = "Eu amo aprender"
+frase_2 = "diariamente"
 print(frase_1 + frase_2)
-
-contador = frase_1.count("Vou")
-print(contador)
-
-frase_maiuscula = frase_1.upper()
-print(frase_maiuscula)
-
-nome_invertido = nome[::-1]
-print(nome_invertido)
-
-frase_separada = frase.split()
-print(frase_separada)
-
-frase_a = frase.count("a")
-print(frase_a)
-
-
-frase_11 = frase. split()[1]
-print(frase_11)
-frase_12 = frase.split()[2] 
-print(frase_12)
-frase_inv = frase_1[::-1]
-print(frase_inv)
+texto = "Vou finzalizar a minha Jornada Python com sucesso"
+texto_count = texto.count("Python")
+print(texto_count)
+nome_inv = nome[::-1]
+print(nome_inv)
+frase_count = frase.count("a")
+print(frase_count)
+frase_rep = frase.replace("a","e")
+print(frase_rep)
+nome_ind = nome.index("Pinheiro")
+print(nome_ind)
+frase_print1 = frase.split()[0]
+print(frase_print1)
+frase_print2 = frase.split()[1]
+print(frase_print2)
+frase_print3 = frase.split()[2]
+print(frase_print3)
+Frase_last = frase[-1]
+print(Frase_last)
+frase_sec = frase[::-1]
+print(frase_sec)
